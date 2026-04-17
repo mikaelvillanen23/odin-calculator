31fcf18da1208aafac73b42591db2b7f5aaf6a52
mikaelvillanen23 <mikael.villanen23@gmail.com>
2026-04-18 00:22:39 +0300

    Add base files for the project
    Script.js also includes some foundations like the basic functions for
    each operation.

---
+++ script.js
@@ -0,0 +1,40 @@
+let firstNum;
+let operator;
+let secondNum;
+
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+function squared(num) {
+  return num * num;
+}
+function squareRoot(num) {
+  // return...
+}
+function cubed(num) {
+  return num * num * num;
+}
+function toPow(num, n) {
+  return num ** n;
+}
+function operate(operator, a, b) {
+  // jotain if operator === "+"...
+}
+function updateFirstNum(input) {
+  firstNum = input;
+}
+function updateOperator(input) {
+  operator = input;
+}
+function updateSecondtNum(input) {
+  secondNum = input;
+}
